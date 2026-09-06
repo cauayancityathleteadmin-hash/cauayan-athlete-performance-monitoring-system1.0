081351db23b6523866231f9ab604ab5833382e35
dacayananjohnpaulo-lgtm <dacayananjohnpaulo@gmail.com>
2026-09-06 21:01:49 +0800

    diagnostic: /account hardcoded props + removed from middleware (temporary)

--- next-app/middleware.js
+++ next-app/middleware.js
@@ -41,8 +41,6 @@ export const config = {
     "/analytics/:path*",
     "/event-plans",
     "/event-plans/:path*",
-    "/account",
-    "/account/:path*",
     "/change-password",
     "/admin",
     "/admin/:path*",
